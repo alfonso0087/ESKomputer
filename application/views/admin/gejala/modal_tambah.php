a5a8e025ccb406675072bad7fc78342d4d752b5b
alfonso0087 <56929086+alfonso0087@users.noreply.github.com>
2019-12-06 22:55:27 +0700

    Ubah modal dibagian tambah+ubah gejala
    Ubah modal dibagian tambah+ubah gejala

--- application/views/admin/gejala/modal_tambah.php
+++ application/views/admin/gejala/modal_tambah.php
@@ -1,5 +1,5 @@
 <div class="modal fade tambah" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
 
             <div class="modal-header">
--- application/views/admin/gejala/modal_ubah.php
+++ application/views/admin/gejala/modal_ubah.php
@@ -1,6 +1,6 @@
 <?php foreach ($gejala as $gjl) : ?>
   <div class="modal fade ubahGejala<?= $gjl['kode_gejala']; ?>" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
